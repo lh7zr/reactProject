867c41bc5b259eaf21131cd1c3a92fc60fbb86cd
lh7zr <jd_best@163.com>
2022-03-30 15:36:10 +0800

    feat: react 学习

--- README.md
+++ README.md
@@ -12,3 +12,10 @@
 - src：主要代码编写文件（非常重要）
 ### react项目注意规则
 - 组件外层包裹原则（组件的最外层要有div包裹，否则会报错）
+- React （响应式设计）通过数据变化改变dom
+- React是禁止直接操作state的
+- 添加样式class时注意不能用class要改成className ，否则控制台会报错: Warning: Invalid DOM property `class`. Did you mean `className`
+### React写JSX的注释
+- {/* 正确注释的写法 */} 快捷键：webstorm编辑器直接按Ctrl+/
+
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React,{Component} from "react";
+import React,{Component,Fragment} from "react";
+import './style.css'
 class App extends Component{
+    constructor(props) {
+        super(props)//调用父类的构造函数，固定写法
+        this.state={
+            inputValue:'', //input中的值
+            list:['林思意','鞠婧祎','陆婷']
+        }
+    }
     render() {
         return (
-            <div>
-                heloo linfeihan
-            </div>
+            <Fragment>
+
+                <div>
+                    <input className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <button onClick={this.addList.bind(this)}> 选择人物 </button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                <li
+                                    key={index+item}
+                                    onClick={this.deleteItem.bind(this)}>{item}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </Fragment>
         )
+    }
+    inputChange(e){
+       this.setState(
+           {inputValue: e.target.value}
+       )
+    }
+    /**添加**/
+    addList(){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue]
+        })
+    }
+    /**删除**/
+    deleteItem(index){
+        console.log(index,'index')
     }
 }
 export  default App
